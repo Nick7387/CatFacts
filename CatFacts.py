99cd246fd9038cad28e9d3867e11ccfd6295eb4a
Nick7387 <39436596+Nick7387@users.noreply.github.com>
2021-03-13 15:27:01 +0400

    Add files via upload

--- CatFacts.py
+++ CatFacts.py
@@ -3,10 +3,58 @@ import requests
 class CatFacts:
 
     def __init__(self):
-        self.base_url = "https://cat-fact.herokuapp.com/facts/"
 
-    def find_facts_random(self):
-        self.res = requests.get(self.base_url+'random/')
+        """api позволяет получать непосредственно факты о животных 
+        и иформацию о корневом пользователе
+        
+        --------------------------------------
+        РАБОТА С ФАКТАМИ
+        
+        Получить факты можно тремя способами: случайный факт, факт по его id, мои факты.
+        заголовок - random
+        заголовок - id
+        заголовок - me
+        
+        заголовок - random
+            facts_random()
+            выдает один факт случайно в формате str
+            
+            
+            facts_random_filter(animals,amount)
+            Метод выдает факты о кокретном животном animals в количестве amount ответов
+            
+            animals: ['cat','dog','snail','horse'] 
+            amount:  максимум 500 фактов
+            
+            Пример: facts_random_filter(['cat','dog'], 200)
+            Метод выдает данные в формате json
+            
+        заголовок - id
+            facts_id()
+                        
+        заголовок - me
+            facts_me()
+        
+        
+        -------------------------------------
+        ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ 
+        
+        Получить информацию о пользователе можно с помощью заголовка users/me
+        расположен в словаре self.users_headers
+        запрос реализуется функцией:
+            
+        current_user_get_info() - метод возвращает данные в формате json
+        
+        """
+        self.base_url = "https://cat-fact.herokuapp.com/"
+        
+        self.base_headers  = {'facts': 'facts', 'users': 'users/me'}
+        self.facts_headers = {'random': 'facts/random','id': 'facts/'+str(id),'me': 'facts/me'} 
+        self.facts_random_headers = {'random': 'facts/random'} 
+        self.users_headers = {'me': 'users/me'}
+
+    def facts_random(self):
+        self.res = requests.get(self.base_url, headers=self.base_headers['users'])
         if self.res:
             print('find_facts_random.Response OK: ', self.res.status_code)
             self.facts=self.res.json().get('text')
@@ -15,28 +63,29 @@ class CatFacts:
             print('find_facts_random.Response Failed: ', self.res.status_code)
             return []
 
-    def find_facts_random_filter(self,animals,amount):
-            animals_data=','.join(animals)
-            params = {'animal_type':animals_data, 'amount':amount}
-            self.res = requests.get(self.base_url+'random',params=params)
-            if self.res:
-                print('find_facts_random_filter.Response OK: ', self.res.status_code)
-                
-                self.facts=[]
-                self.users=[]
-                self.dict_data = {}
-                
-                for i in self.res.json():
-                    self.facts.append(i.get('text'))
-                    self.users.append(i.get('user'))
-                    
-                    if len(self.facts)>0:
-                        
-                        for i in range(len(self.facts)):
-                            self.dict_data[self.users[i]] = self.facts[i]
-                    
-                return self.dict_data
-            else:
-                print('find_facts_random_filter.Response Failed: ', self.res.status_code)
-                return []
+    def facts_random_filter(self,animals,amount):
+        url=self.base_url + self.facts_headers['random']
+        animals_data=','.join(animals)
+        params = {'animal_type':animals_data, 'amount':amount}
+        self.res = requests.get(url, params=params)
+        print(self.res.url)
+        if self.res:
+            print('find_facts_random_filter.Response OK: ', self.res.status_code)
+            self.facts=self.res.json()              
+            return self.facts
+        else:
+            print('find_facts_random_filter.Response Failed: ', self.res.status_code)
+            return False
+            
+    def facts_id(self):
+        return self
+    
+    def facts_me(self):
+        return self
+            
+    def current_user_get_info(self):
+        self.base_url = "https://cat-fact.herokuapp.com"
+        self.res = requests.get(self.base_url, headers=self.base_headers['users'])
+        self.me_info=self.res.json()
+        return self.me_info
 
--- Test.py
+++ Test.py
@@ -2,15 +2,29 @@ import requests
 from CatFacts import CatFacts
 
 animals=['cat','dog','snail','horse']
-amount=500
+amount=300
 
 
 
 def test_all_facts(animals, amount):
     cat_facts_data = CatFacts()
     #print(len(cat_facts_data.find_facts_random_filter(animals, amount)))
-    for i in animals:
-        a=cat_facts_data.find_facts_random_filter([i], amount)
-        return a
+    return cat_facts_data.facts_random_filter(animals, amount)
+    # for i in animals:
+    #     a=cat_facts_data.find_facts_random_filter([i], amount)
+    #     return a
     
 b=test_all_facts(animals, amount)
+users=[]
+for i in range(len(b)):
+    users.append(b[i]['user'])
+
+uniqNames = sorted(set(users)) #remove duplicate words and sort
+d = {}    
+
+for name in uniqNames:
+    d[name] = users.count(name) 
+    
+print(d)
+    
+s=list(d.items())
